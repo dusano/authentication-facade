ac88d6b95f6bf92ac87b94b14f12cfb57003930f
dusano <dusan.omercevic@zemanta.com>
2014-02-14 16:17:59 +0100

    target_url can be a long string (e.g. in case of graphite urls)

--- facade/models.py
+++ facade/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Mapping(models.Model):
 	plug = models.CharField(max_length = 255, unique = True)
-	target_url = models.CharField(max_length = 700, db_index = True)
+	target_url = models.TextField()
 	index_page = models.BooleanField(default=False, db_index=True)
 	group = models.CharField(max_length = 255, blank=True)
