caae660c7f6c329ccfa27b3eac233fe77a226658
dusano <dusan.omercevic@zemanta.com>
2014-02-14 13:01:13 +0100

    Making login required for bridge

--- facade/views.py
+++ facade/views.py
@@ -1,5 +1,8 @@
 from django import http
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def bridge(request, target):
 	return http.HttpResponse("<html><body>Target: %s</body></html>" % target)
