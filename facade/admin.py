76b26245c2d3aa265d0e2c303e5fdfe0992be8de
dusano <dusan.omercevic@zemanta.com>
2014-02-23 17:45:43 +0100

    Making all data visible in Mapping admin view by truncating fields

--- facade/admin.py
+++ facade/admin.py
@@ -3,6 +3,19 @@ from facade.models import Mapping
 
 
 class MappingAdmin(admin.ModelAdmin):
-    list_display = ('plug', 'target_url', 'index_page', 'group')
+	list_display = ('plug', '_pattern', '_target_url', 'index_page', 'group')
+
+	def _pattern(self, obj):
+		if obj.pattern:
+			return obj.pattern[:50]
+		else:
+			return obj.pattern
+
+	def _target_url(self, obj):
+		if obj.target_url:
+			return obj.target_url[:50]
+		else:
+			return obj.target_url
+
 
 admin.site.register(Mapping, MappingAdmin)
